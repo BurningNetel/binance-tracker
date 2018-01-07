d1c7327b1fe069c399937555727eea0c2c375b83
BurningNetel <joepseuren1@gmail.com>
2018-01-07 13:00:35 +0100

    Refactor

--- runner.py
+++ runner.py
@@ -1,7 +1,7 @@
 import pymongo
 import sys
 import binance
-import threading 
+import threading
 import time
 
 MONGODB_PORT = 27017
@@ -13,53 +13,55 @@ pricesCollection = db.prices
 
 
 class RepeatedTimer(object):
-  def __init__(self, interval, function, *args, **kwargs):
-    self._timer = None
-    self.interval = interval
-    self.function = function
-    self.args = args
-    self.kwargs = kwargs
-    self.is_running = False
-    self.next_call = time.time()
-    self.start()
+    """Runs a function at every interval without drift due to execution time of the function."""
+    def __init__(self, interval, function, *args, **kwargs):
+        self._timer = None
+        self.interval = interval
+        self.function = function
+        self.args = args
+        self.kwargs = kwargs
+        self.is_running = False
+        self.next_call = time.time()
+        self.start()
 
-  def _run(self):
-    self.is_running = False
-    self.start()
-    self.function(*self.args, **self.kwargs)
+    def _run(self):
+        self.is_running = False
+        self.start()
+        self.function(*self.args, **self.kwargs)
 
-  def start(self):
-    if not self.is_running:
-      self.next_call += self.interval
-      self._timer = threading.Timer(self.next_call - time.time(), self._run)
-      self._timer.start()
-      self.is_running = True
+    def start(self):
+        if not self.is_running:
+            self.next_call += self.interval
+            self._timer = threading.Timer(self.next_call - time.time(), self._run)
+            self._timer.start()
+            self.is_running = True
 
-  def stop(self):
-    self._timer.cancel()
-    self.is_running = False
+    def stop(self):
+        self._timer.cancel()
+        self.is_running = False
 
 
-def getBalances():
+def get_balances():
     balances = binance.balances()
     prices = binance.prices()
-    currentDateTime = time.time()
-    dataBalances = {"balances":balances, "date":currentDateTime}
-    dataPrices = {"prices":prices, "date":currentDateTime}
-    balancesCollection.insert_one(dataBalances)
-    pricesCollection.insert_one(dataPrices)
+    epoch = time.time()
+    data_balances = {"balances": balances, "date": epoch}
+    data_prices = {"prices": prices, "date": epoch}
+    balancesCollection.insert_one(data_balances)
+    pricesCollection.insert_one(data_prices)
     print("inserted data")
 
-def main():
-    args =  sys.argv[1:]
-    secret = args[0]
-    apikey = args[1]
-    binance.set(apikey, secret)
 
-    rt = RepeatedTimer(60, getBalances)
+def main():
+    args = sys.argv[1:]
+    secret_key = args[0]
+    api_key = args[1]
+    binance.set(api_key, secret_key)
+
+    rt = RepeatedTimer(60, get_balances)
     rt.start()
     print("Started!")
 
+
 if __name__ == "__main__":
     main()
-
